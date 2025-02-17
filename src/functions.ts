a8ebd6000c2551951e7ddaa7b7d27fb608c4538f
Maddox Florez <maddox@udel.edu>
2025-02-17 11:11:39 -0500

    finished all functions

--- src/functions.ts
+++ src/functions.ts
@@ -41,7 +41,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.slice(-1) === "?";
 }
 
 /**
@@ -50,5 +50,11 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word.toLowerCase() === "yes") {
+        return true;
+    } else if (word.toLowerCase() === "no") {
+        return false;
+    } else {
+        return null;
+    }
 }
