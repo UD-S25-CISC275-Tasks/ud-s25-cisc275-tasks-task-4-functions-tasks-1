2f4d236fe595f93da3bd2b13c7a3780aa8c66f6a
Ember Kerstetter <emberk@udel.edu>
2025-02-18 18:07:02 -0500

    Finished shout, isQ, and yesNo functions. All tests pass.

--- src/functions.ts
+++ src/functions.ts
@@ -29,7 +29,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return message.toUpperCase() + "!";
 }
 
 /**
@@ -37,7 +37,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message[message.length - 1] === "?";
 }
 
 /**
@@ -46,5 +46,10 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word.toLowerCase() === "yes") {
+        return true;
+    } else if (word.toLowerCase() === "no") {
+        return false;
+    }
+    return null;
 }
